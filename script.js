dbababc488d87d0c1c336836372503e2b063cee4
Will Richardson <rich1051@umn.edu>
2023-05-02 20:10:48 -0500

    Verified sourcing, created Addition Function

---
+++ script.js
@@ -0,0 +1,15 @@
+//Test to make sure .js sourced correctly
+console.log ('Hello, Instructor!');
+
+//PROBLEM #1: Addition
+function additionFunction(num1, num2) {
+    return num1 + num2
+}
+
+console.log('The sum of 22 and 55 is:', additionFunction(22, 55));
+console.log('The sum of 187 and 13 is:', additionFunction(187, 13));
+
+//PROBLEM #1: Conditional
+function conditionalFunction(num1, num2) {
+    return 
+}
